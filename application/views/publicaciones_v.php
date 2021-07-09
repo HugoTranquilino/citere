a2c2ba69fc430be71c3272e01421e0aa6105e780
Hugo <hugotranquilino@gmail.com>
2021-07-09 17:59:36 -0500

    inicie la seccion de publicaciones, aun en desarrollo

--- application/views/publicaciones_v.php
+++ application/views/publicaciones_v.php
@@ -1,4 +1,4 @@
-<div class="flex">
+<!-- <div class="flex">
     <div class="column">
         <img class="img_public" src="<?php echo base_url();?>assets/img/p-1.jpg" alt="">
         <img class="img_public" src="<?php echo base_url();?>assets/img/p-2.jpg" alt="">
@@ -16,4 +16,161 @@
         <img class="img_public" src="<?php echo base_url();?>assets/img/p-8.jpg" alt="">
         <img class="img_public" src="<?php echo base_url();?>assets/img/p-1.jpg" alt="">
     </div>
+</div> -->
+
+<div class="container publicaciones">
+	<div class="row">
+		<!-- ===== Section citere opina ========== -->
+		<div class="col-md-8" style="background-color: red;">
+			<!-- ======== Publicaciones ======== -->
+			<ul class="slider">
+				<li id="slide1">
+					<img src="<?php echo base_url();?>assets/img/publicaciones/n1.jpg" />
+				</li>
+				<li id="slide2">
+					<img src="<?php echo base_url();?>assets/img/publicaciones/n2.jpg" />
+				</li>
+				<li id="slide3">
+					<img src="<?php echo base_url();?>assets/img/publicaciones/n3.jpg" />
+				</li>
+			</ul>
+
+			<!-- ===== Options menu ======= -->
+			<ul class="menu">
+				<li>
+					<a href="#slide1">1</a>
+				</li>
+				<li>
+					<a href="#slide2">2</a>
+				</li>
+				<li>
+					<a href="#slide3">3</a>
+				</li>
+			</ul>
+		</div>
+		<!-- ======= End Citere opina =========== -->
+
+		<div class="col-md-4">
+			<div class="row">
+				<!-- ======= Section Economia ======== -->
+				<div class="content-slider-sm" style="background-color: orange;">
+					<!-- ======== Publicaciones ======== -->
+					<ul class="slider-sm">
+						<li id="slide4">
+							<img src="<?php echo base_url();?>assets/img/publicaciones/e1.jpg" />
+						</li>
+						<li id="slide5">
+							<img src="<?php echo base_url();?>assets/img/publicaciones/e2.jpg" />
+						</li>
+						<li id="slide6">
+							<img src="<?php echo base_url();?>assets/img/publicaciones/e3.jpg" />
+						</li>
+					</ul>
+
+					<!-- ===== Options menu ======= -->
+					<ul class="menu econonia">
+						<li>
+							<a href="#slide4">4</a>
+						</li>
+						<li>
+							<a href="#slide5">5</a>
+						</li>
+						<li>
+							<a href="#slide6">6</a>
+						</li>
+					</ul>
+				</div>
+				<!-- ======= End Section Economia ======== -->
+
+				<!-- ======= Section Negocios ======== -->
+				<div class="content-slider-sm" style="background-color: green;">
+					<!-- ======== Publicaciones ======== -->
+					<ul class="slider-sm">
+						<li id="slide7">
+							<img src="<?php echo base_url();?>assets/img/publicaciones/i1.jpg" />
+						</li>
+						<li id="slide8">
+							<img src="<?php echo base_url();?>assets/img/publicaciones/i2.jpg" />
+						</li>
+						<li id="slide9">
+							<img src="<?php echo base_url();?>assets/img/publicaciones/i3.jpg" />
+						</li>
+					</ul>
+
+					<!-- ===== Options menu ======= -->
+					<ul class="menu negocios">
+						<li>
+							<a href="#slide7">7</a>
+						</li>
+						<li>
+							<a href="#slide8">8</a>
+						</li>
+						<li>
+							<a href="#slide9">9</a>
+						</li>
+					</ul>
+				</div>
+				<!-- ======= End Section Negocios ======== -->
+
+				<!-- ======= Sitios de Interes ======== -->
+				<div class="content-slider-sm" style="background-color: purple;">
+					<!-- ======== Publicaciones ======== -->
+					<ul class="slider-sm">
+						<li id="slide10">
+							<img src="<?php echo base_url();?>assets/img/publicaciones/a1.jpg" />
+						</li>
+						<li id="slide11">
+							<img src="<?php echo base_url();?>assets/img/publicaciones/a2.jpg" />
+						</li>
+						<li id="slide12">
+							<img src="<?php echo base_url();?>assets/img/publicaciones/a3.jpg" />
+						</li>
+					</ul>
+					<!-- ===== Options menu ======= -->
+					<ul class="menu interes">
+						<li>
+							<a href="#slide10">10</a>
+						</li>
+						<li>
+							<a href="#slide11">11</a>
+						</li>
+						<li>
+							<a href="#slide12">12</a>
+						</li>
+					</ul>
+				</div>
+				<!-- ======= End Sitios de Interes ======== -->
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<div class="container publicaciones">
+
+	<!-- <ul class="slider">
+		<li id="slide1">
+			<img src="<?php echo base_url();?>assets/img/publicaciones/n1.jpg" />
+		</li>
+		<li id="slide2">
+			<img src="<?php echo base_url();?>assets/img/publicaciones/n2.jpg" />
+		</li>
+		<li id="slide3">
+			<img src="<?php echo base_url();?>assets/img/publicaciones/n3.jpg" />
+		</li>
+	</ul> -->
+
+
+	<!-- <ul class="menu">
+		<li>
+			<a href="#slide1">1</a>
+		</li>
+		<li>
+			<a href="#slide2">2</a>
+		</li>
+		<li>
+			<a href="#slide3">3</a>
+		</li>
+	</ul> -->
+
 </div>
