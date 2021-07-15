28359e9108eb508b401bad9a2a73a61993dc15d7
Hugo <hugotranquilino@gmail.com>
2021-07-15 16:25:48 -0500

    actualizacion del circulo de valores - seccion nosotros

--- application/views/nosotros_v.php
+++ application/views/nosotros_v.php
@@ -7,32 +7,32 @@
 				<div class="row">
 					<div class="col-md-6 col-sm-12 col-xs-12">
 						<h2>Proyección</h2>
-						<p>En CITERE, nuestra prioridad es Detonar “Negocios en Movimiento”. Nos
-							especializamos en impulsar los cambios externos y de operación interna
+						<p>En CITERE, nuestra prioridad es Detonar “Negocios en Movimiento”. Nos
+							especializamos en impulsar los cambios externos y de operación interna
 							en beneficio de tu compañía.</p>
 						<p>Nos especializamos en la asesoría y
-							acompañamiento fiscal, administrativo, corporativo y patrimonial; dirigido a negocios
+							acompañamiento fiscal, administrativo, corporativo y patrimonial; dirigido a negocios
 							privados, empresarios
 							y entidades públicas.</p>
 						<p>Te garantizamos que estarás al día
 							con Hacienda, pero lo que más nos
 							interesa es que tomes las mejores
-							decisiones patrimoniales, empresariales y de negocios.</p>
-						<p>Nuestro objetivo es lograr la confianza de nuestros clientes para que se
+							decisiones patrimoniales, empresariales y de negocios.</p>
+						<p>Nuestro objetivo es lograr la confianza de nuestros clientes para que se
 							sientan seguros y satisfechos con
 							nuestros servicios.</p>
 					</div>
 					<div class="col-md-6 col-sm-12 col-xs-12 filosofia">
 						<h2>Filosofía</h2>
 						<p>CITERE cuenta con un comprometido
-							Equipo de Trabajo integrado por Profesionales de Alto Nivel, con especialidad en materia
+							Equipo de Trabajo integrado por Profesionales de Alto Nivel, con especialidad en materia
 							Fiscal y Financiera, en
 							búsqueda permanente de Soluciones
 							de Negocios, en los que la Integridad y
 							Disciplina son valores fundamentales;
 							estamos convencidos de que nuestro
 							enfoque de Servicio Personalizado es
-							un factor de éxito para lograr la excelencia de nuestros servicios</p>
+							un factor de éxito para lograr la excelencia de nuestros servicios</p>
 					</div>
 				</div>
 				<div class="row">
@@ -40,8 +40,8 @@
 						<h2>Misión</h2>
 						<p>Proporcionar a nuestros clientes
 							soluciones fiscales y financieras,
-							sostenibles en el tiempo, que les garanticen una gestión empresarial
-							eficiente mediante prácticas y metodologías adecuadas a las actuales
+							sostenibles en el tiempo, que les garanticen una gestión empresarial
+							eficiente mediante prácticas y metodologías adecuadas a las actuales
 							disposiciones fiscales y en el ámbito
 							financiero, así como con cualquier
 							variación jurídica que impacte en el
@@ -60,42 +60,59 @@
 
 				<!-- ======= Graph of values =========== -->
 				<div class="container circulo">
-					<div class="circulo-valores">
-						<h3>valores</h3>                                                                                                                                             
-						<img class="c1" id="c1" src="<?php echo base_url();?>assets/img/nosotros/c-1.png" alt="Valor de la Integridad">
-						<img class="c2" id="c2" src="<?php echo base_url();?>assets/img/nosotros/c-2.png" alt="Valor de la Autocritica">
-						<img class="c3" id="c3" src="<?php echo base_url();?>assets/img/nosotros/c-3.png" alt="Valor de la Disiplina">
-						<img class="c4" id="c4" src="<?php echo base_url();?>assets/img/nosotros/c-4.png" alt="Valor de la Cercania">
+					<!-- Description of options above -->
+					<div class="col-md-12 row items-top">
+						<div class="col-md-6 col-lg-6">
+							<p class="description_value" id="description_1" style="display: none;">
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet soluta maiores eos,
+								temporibus aperiam suscipit accusamus eius reprehenderit! Quae exercitationem vel labore
+								iusto?
+							</p>
+							<img id="f1" src="<?php echo base_url();?>assets/img/nosotros/item-1.png"
+								alt="Identificador de Integridad" style="display: none;">
+						</div>
+						<div class="col-md-6 col-lg-6">
+							<p class="description_value" id="description_2" style="display: none;">
+								Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia tenetur cupiditate odio
+								officia consequuntur!
+							</p>
+							<img id="f2" src="<?php echo base_url();?>assets/img/nosotros/item-2.png"
+								alt="Identificador de Integridad" style="display: none;">
+						</div>
 					</div>
-					
-					<!-- options description -->
-					<img class="f1" id="f1" src="<?php echo base_url();?>assets/img/nosotros/item-1.png" alt="Identificador de Integridad"
-						style="display: none;">
-					<p id="description_1" style="display: none;">
-						Siempre cumplimos los compromisos con nuestros clientes,
-						socios y colaboradores. Valoramos por sobre todo obtener su
-						confianza por nuestra actitud
-						de servicio y ética profesional
-					</p>
 
-					<img class="f2" id="f2" src="<?php echo base_url();?>assets/img/nosotros/ff1.png" alt="Identificador de Autocritica"
-						style="display: none;">
-					<p id="description_2" style="display: none;">
-						Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia tenetur cupiditate odio officia consequuntur!
-					</p>
+					<!-- Circle of values -->
+					<div class="circulo-valores">
+						<h3>VALORES</h3>
+						<img class="c1" id="c1" src="<?php echo base_url();?>assets/img/nosotros/c-1.png"
+							alt="Valor de la Integridad">
+						<img class="c2" id="c2" src="<?php echo base_url();?>assets/img/nosotros/c-2.png"
+							alt="Valor de la Autocritica">
+						<img class="c3" id="c3" src="<?php echo base_url();?>assets/img/nosotros/c-3.png"
+							alt="Valor de la Disiplina">
+						<img class="c4" id="c4" src="<?php echo base_url();?>assets/img/nosotros/c-4.png"
+							alt="Valor de la Cercania">
+					</div>
 
-					<img class="f3" id="f3" src="<?php echo base_url();?>assets/img/nosotros/ff1.png" alt="Identificador de Disiplina"
-						style="display: none;">
-					<p id="description_3" style="display: none;">
-						Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia tenetur cupiditate odio officia consequuntur!
-					</p>
-
-					<img class="f4" id="f4" src="<?php echo base_url();?>assets/img/nosotros/ff1.png" alt="Identificador de Cercania"
-						style="display: none;">
-					<p id="description_4" style="display: none;">
-						Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia tenetur cupiditate odio officia consequuntur!
-					</p>
-					
+					<!-- Options description below -->
+					<div class="col-md-12 row item-down">
+						<div class="col-md-6 col-lg-6">
+							<p class="description_value" id="description_4" style="display: none;">
+								Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia tenetur cupiditate odio
+								officia consequuntur!
+							</p>
+							<img id="f4" src="<?php echo base_url();?>assets/img/nosotros/item-4.png"
+								alt="Identificador de Integridad" style="display: none;">
+						</div>
+						<div class="col-md-6 col-lg-6">
+							<p class="description_value" id="description_3" style="display: none;">
+								Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia tenetur cupiditate odio
+								officia consequuntur!
+							</p>
+							<img id="f3" src="<?php echo base_url();?>assets/img/nosotros/item-3.png"
+								alt="Identificador de Integridad" style="display: none;">
+						</div>
+					</div>
 				</div>
 				<!-- ======= End Graph of values =========== -->
 			</div>
